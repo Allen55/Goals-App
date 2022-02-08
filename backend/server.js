9ffceff758000f73fd554c3baf2d52dc36affd90
Allen Harper <aharper1609@gmail.com>
2022-02-07 18:30:34 -0600

    Added DB and mongoose.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db');
 const port = process.env.PORT || 8000;
+
+connectDB();
 const app = express();
 
 app.use(express.json());
@@ -9,5 +13,6 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use(errorHandler);
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
