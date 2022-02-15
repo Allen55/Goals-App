66d334b062cb5b5762e2b66d6779bf21740265bf
Allen Harper <aharper1609@gmail.com>
2022-02-15 17:53:30 -0600

    added auth middleware and protect functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const { restart } = require('nodemon');
+const { json } = require('body-parser');
 
 
 // @desc     Register new user
@@ -36,9 +37,10 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
-        })
-    } else{
+            email: user.email,
+            token: generateToken(user._id)
+    })
+    } else {
         res.status(400)
         throw new Error("invalid")
     }
@@ -50,17 +52,43 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route    POST /api/users/login
 // @access   Public
 const loginUser =  asyncHandler(async (req, res) => {
+    const {email, password} = req.body;
+
+    //check for user email
+    const user = await User.findOne({email});
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400);
+        throw new Error("Invalid credentials");
+    }
     res.json({message: "Login User"})
 })
 
 // @desc     Get user data
-// @route    POST /api/users/me
-// @access   Public
+// @route    Get /api/users/me
+// @access   Private
 const getMe =  asyncHandler (async (req, res) => {
-    res.json({message: "User data display"})
+    const { _id, name, email } = await User.findById(req.user.id);
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    })
 })
 
-
+//Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 module.exports = {
     registerUser,
